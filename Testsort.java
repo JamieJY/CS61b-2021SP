768683c55a0f30597b2258ae88e697988896bd30
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-07-21 11:08:48 -0400

    lecture3 new way to test ad-hoc testing

---
+++ Testsort.java
@@ -0,0 +1,34 @@
+package testsort;
+
+/* tests the sort method*/
+public class Testsort {
+	
+	/* helper method */
+	public static void testSort() {
+		String[] input = {"i","have","an","egg"};
+		String[] expected = {"an","egg","have","i"};
+		
+		Sort.sort(input);
+		
+		for(int i=0;i<input.length;i++) {
+			if(!input[i].equals(expected[i])) {
+			    System.out.println("mismatch in position"+i+",expected:"+expected[i]+",but got:"+input[i]);
+			    return;
+			}
+		}
+		if(!java.util.Arrays.equals(input,expected)) {
+			System.out.println("error!something is wrong with Sort.sort");
+		}
+	}
+	
+	
+	public class Sort{
+		/* sort strings destructively */
+		public static void sort(String[] x) {
+			
+		}
+	}
+	public static void main(String[] args) {
+		testSort();
+	}
+}
