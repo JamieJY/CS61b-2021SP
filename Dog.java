2d3e8b5e31e5b7436a99e7080b2706b783b8c55c
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-08-28 16:01:14 -0400

    10subtype polymorphism

--- Dog.java
+++ Dog.java
@@ -1,46 +1,18 @@
-package dog;
+public class Dog implements OurComparable{
+    private String name;
+    private int size;
 
-public class Dog {
+    public Dog(String n,int s){
+        name = n;
+        size = s;
+    }
 
-		public int weightInPounds;
-		public static String binomen = "Canis familiaris";
+    public void bark(){
+        System.out.println(name + "says : bark");
+    }
 
-		/** One integer constructor for dogs. */
-		public Dog(int w) {
-			weightInPounds = w;
-		}
-
-		public void makeNoise() {
-			if (weightInPounds < 10) {
-				System.out.println("yip!");
-			} else if (weightInPounds < 30) {
-				System.out.println("bark.");
-			} else {
-				System.out.println("woooof!");
-			}
-		}
-
-		public static Dog maxDog(Dog d1,Dog d2) {
-			if (d1.weightInPounds > d2.weightInPounds) {
-				return d1;
-			}
-			return d2;
-		}	
-
-
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Dog d1 = new Dog(15);
-		Dog d2 = new Dog(100);
-		Dog bigger = Dog.maxDog(d1, d2);
-		//bigger.makeNoise();                   
-		
-		//Dog bigger = d1.maxDog(d2);
-		bigger.makeNoise();
-		
-		
-	}
-
+    public int compareTo(Object o){
+        Dog uddaDog = (Dog) o;
+        return this.size-uddaDog.size;
+    }
 }
